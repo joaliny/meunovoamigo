2b8d79fa6fe7525d93309cbb1b902ccb6a1d7dd8
Joaliny F. <141338967+joaliny@users.noreply.github.com>
2025-08-20 18:55:08 -0400

    criação da estrutura da classe pet

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Pet(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.CharField(max_length=50)
+    raca = models.CharField(max_length=100, blank=True)
+    descricao = models.TextField(blank=True)
+    foto = models.ImageField(upload_to='pets/', blank=True)
+
+    def __str__(self):
+        return self.nome
